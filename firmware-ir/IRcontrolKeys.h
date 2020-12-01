3e38f66e100e86e828b0f5761ca00838e92fbb89
meycasim <75312080+meycasim@users.noreply.github.com>
2020-12-01 15:10:28 +0300

    Update IRcontrolKeys.h

--- firmware-ir/IRcontrolKeys.h
+++ firmware-ir/IRcontrolKeys.h
@@ -21,23 +21,23 @@
 #define KEY_CONTROL   16
 
 constexpr unsigned long ir_control_keys[]={
-  0xFF0DF2, // key 0
-  0xFF10EF, // key 1
-  0xFF11EE, // key 2
-  0xFF12ED, // key 3
-  0xFF14EB, // key 4
-  0xFF15EA, // key 5
-  0xFF16E9, // key 6
-  0xFF18E7, // key 7
-  0xFF19E6, // key 8
-  0xFF1AE5, // key 9
-  0xFF01FE, // forward, arrow up key
-  0xFF09F6, // backward, arrow down key
-  0xFF06F9, // right, arrow right key
-  0xFF04FB, // left, arrow left key 
-  0xFF05FA, // stand, middle key
-  0xFF02FD, // auto, menu key, 1st column most rightnest key
-  0xFF00FF  // control, power key, 1st column most left key
+  0xFF9867, // key 0
+  0xFFA25D, // key 1
+  0xFF629D, // key 2
+  0xFFE21D, // key 3
+  0xFF22DD, // key 4
+  0xFF02FD, // key 5
+  0xFFC23D, // key 6
+  0xFFE01F, // key 7
+  0xFFA857, // key 8
+  0xFF906F, // key 9
+  0xFF18E7, // forward, arrow up key
+  0xFF4AB5, // backward, arrow down key
+  0xFF5AA5, // right, arrow right key
+  0xFF10EF, // left, arrow left key 
+  0xFF38C7, // stand, middle key
+  0xFF6897, // auto, menu key, 1st column most rightnest key
+  0xFFB04F  // control, power key, 1st column most left key
 };
 
 #endif
